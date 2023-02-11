ccc0c7d6385a40f6050577f8c4d50ffd843f0195
Henrique Silva <113844247+Henrsilva12@users.noreply.github.com>
2023-02-11 14:23:28 -0300

    Add files via upload

---
+++ src/olamundo/OlaMundo.java
@@ -0,0 +1,17 @@
+package olamundo;
+
+public class OlaMundo {
+
+    public static void main(String[] args) { // psvm + tab = cria esse código
+        
+        //System.out.print("Olá Mundo!");
+        //se digitar sout e apertar tab, ele digita " System.out.println automaticamente que é o escreva de algoritmo
+        //System.out.println(""); sout + tab
+        
+        System.out.println("Olá Mundo!");
+        
+    }
+        
+        
+                  
+}
